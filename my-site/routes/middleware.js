8c45c9b7df75c3d16e9068de1ff52c7ef94b69ac
Soohyun Kim <shkim7534@gmail.com>
2017-09-23 14:52:34 -0700

    login form now working

--- my-site/routes/middleware.js
+++ my-site/routes/middleware.js
@@ -43,7 +43,7 @@ exports.initLocals = function (req, res, next) {
 	locals.basedir = keystone.get('basedir');
 
 	locals.page = {
-		title: 'SydJS',
+		title: 'UBC Agrifood Database',
 		path: req.url.split('?')[0], // strip the query - handy for redirecting back to the page
 	};
 
