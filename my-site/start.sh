ba4ffb993f08a3fce2ac7ce6c1891bc089a30a11
Justin Lee <justinlee0022@gmail.com>
2017-10-24 14:08:43 -0700

    revert back to pm2

--- my-site/start.sh
+++ my-site/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# pm2 delete keystone
+pm2 delete keystone
 
 cd my-site
 
 source .env
 
 npm install
-node keystone
-# pm2 start keystone.js
+# node keystone
+pm2 start keystone.js
+pm2 logs
